adda53c752883be1751e94ccb7db3e443de23ed4
Igori4 <igorpilyak@gmail.com>
2019-04-06 00:22:19 +0300

    create phones component

---
+++ js/phones/components/phones-catalog.js
@@ -0,0 +1,38 @@
+
+export default class PhonesCatalog {
+    constructor({element, phones = []}) {
+        this._element = element;
+        this._phones = phones;
+        this._render();
+    }
+
+    _render() {
+      console.log(this._phones)
+        this._element.innerHTML = `
+        <ul class="phones">
+          ${
+
+            this._phones.map(phone => `
+            <li class="thumbnail">
+            <a href="#!/phones/motorola-xoom-with-wi-fi" class="thumb">
+              <img alt="Motorola XOOM™ with Wi-Fi" src="${phone.imageUrl}">
+            </a>
+
+            <div class="phones__btn-buy-wrapper">
+              <a class="btn btn-success">
+                Add
+              </a>
+            </div>
+
+            <a href="#!/phones/motorola-xoom-with-wi-fi">${phone.name}</a>
+            <p>${phone.snippet}</p>
+            </li>
+              `
+            ).join('')
+          }
+
+        
+      </ul>
+        `
+    }
+}
